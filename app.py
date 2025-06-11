0a1261bec58a0aa5840cf6f8e1088d8279d548e6
Manjot Kaur <manjotkaur@gmail.com>
2025-06-11 12:50:08 +0530

    Added working crystal visualizer app

--- app.py
+++ app.py
@@ -1,37 +1,37 @@
 import streamlit as st
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 
-def draw_unit_cells_with_atoms(ax, points, min_coord=0, max_coord=2):
-    base_cube_points = [
-        (0, 0, 0), (1, 0, 0), (1, 1, 0), (0, 1, 0),
-        (0, 0, 1), (1, 0, 1), (1, 1, 1), (0, 1, 1)
-    ]
+def draw_unit_cell(structure):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
-    for i in range(min_coord, max_coord):
-        for j in range(min_coord, max_coord):
-            for k in range(min_coord, max_coord):
-                atom_in_cell = any(
-                    i <= x < i + 1 and j <= y < j + 1 and k <= z < k + 1
-                    for x, y, z in points
-                )
-                if atom_in_cell:
-                    cube_points = [(x + i, y + j, z + k) for x, y, z in base_cube_points]
-                    faces = [
-                        [cube_points[0], cube_points[1], cube_points[5], cube_points[4]],
-                        [cube_points[2], cube_points[3], cube_points[7], cube_points[6]],
-                        [cube_points[0], cube_points[4], cube_points[7], cube_points[3]],
-                        [cube_points[1], cube_points[5], cube_points[6], cube_points[2]],
-                        [cube_points[0], cube_points[1], cube_points[2], cube_points[3]],
-                        [cube_points[4], cube_points[5], cube_points[6], cube_points[7]],
-                    ]
-                    face_colors = 'lightblue'
-                    for face in faces:
-                        poly = Poly3DCollection([face], alpha=0.5, edgecolor='black', linewidths=0.5, facecolor=face_colors)
-                        ax.add_collection3d(poly)
+    # Cube edges
+    for s, e in zip([[0,0,0],[1,0,0],[1,1,0],[0,1,0],[0,0,0],
+                     [0,0,1],[1,0,1],[1,1,1],[0,1,1],[0,0,1]],
+                    [[1,0,0],[1,1,0],[0,1,0],[0,0,0],[0,0,1],
+                     [1,0,1],[1,1,1],[0,1,1],[0,0,1],[0,0,0]]):
+        ax.plot3D(*zip(s,e), color="gray", lw=1)
 
-def plot_crystal_structure(ax, structure):
-    if structure == "BCC":
-        points = points = [(0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1)]
+    # Atom positions
+    if structure == "Simple Cubic":
+        points = [(0,0,0)]
+    elif structure == "BCC":
+        points = [(0,0,0), (0.5,0.5,0.5)]
+    elif structure == "FCC":
+        points = [(0,0,0), (0.5,0.5,0), (0.5,0,0.5), (0,0.5,0.5)]
 
+    for p in points:
+        ax.scatter(*p, color='red', s=100)
+
+    ax.set_xlim([0,1])
+    ax.set_ylim([0,1])
+    ax.set_zlim([0,1])
+    ax.set_box_aspect([1,1,1])
+    st.pyplot(fig)
+
+# Streamlit app
+st.title("Crystal Structure Visualizer")
+
+structure = st.selectbox("Select a crystal structure", ["Simple Cubic", "BCC", "FCC"])
+draw_unit_cell(structure)
